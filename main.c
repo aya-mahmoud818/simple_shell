69a2ab3ede26413fe73600948708e53f52a1834e
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-04-18 19:27:32 -0700

    infinite loop

--- main.c
+++ main.c
@@ -1,18 +1,37 @@
 #include "main.h"
+/**
+ * main -entery
+ *
+ * @argc: arguments count
+ * @argv: vector
+ *
+ * Return: 0
+ */
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 	char *prompt = "simpleshell $";
 	size_t n = 0;
 	char *line;
+	ssize_t num_chars;
 
 	(void)argc;
 	(void)argv;
 
+	while (1)
+	{
 	printf("%s", prompt);
-	getline(&line, &n, stdin);
+
+	num_chars = getline(&line, &n, stdin);
+	if (num_chars == -1)
+	{
+		printf("exit\n");
+		return (-1);
+	}
+
 	printf("%s\n", line);
 
 	free(line);
+	}
 	return (0);
 }
