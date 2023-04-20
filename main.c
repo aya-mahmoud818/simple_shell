2241f847ed9f1e06d7b2e0700bb6410f92b36a38
aya-mahmoud818 <ayamahmoud818@gmail.com>
2023-04-19 17:09:44 -0700

    update

--- main.c
+++ main.c
@@ -2,68 +2,66 @@
 /**
  * main -entery
  *
- * @argc: arguments count
- * @argv: vector
- *
  * Return: 0
  */
 
-int main(void){
-    char *prompt = "simpleshell $";
-    char *lineptr = NULL, *lineptr_copy = NULL;
-    size_t n = 0;
-    ssize_t num_chars; 
-    char *token;
-    const char *delim = " \n";
-    char **argv;
-    int i = 0; 
-    int n_tokens = 0;
+int main(void)
+{
+	char *prompt = "simpleshell $";
+	char *lineptr = NULL, *lineptr_copy = NULL;
+	size_t size = 0;
+	ssize_t num_chars;
+	char *token;
+	const char *delim = " \n";
+	char **strings;
+	int i = 0;
+	int num_tokens = 0;
 
-   
-    printf("$ ");
+	printf("%s", prompt);
+	num_chars = getline(&lineptr, &size, stdin);
+	lineptr_copy = malloc(sizeof(char) * num_chars);
 
-    num_chars = getline(&lineptr, &n, stdin);
+	if (lineptr_copy == NULL)
+	{
+		perror("tsh: memory allocation error");
+		return (-1);
+	}
 
-    lineptr_copy = malloc(sizeof(char) * num_chars);
+	strcpy(lineptr_copy, lineptr);
 
-    if (lineptr_copy == NULL){
-        perror("tsh: memory allocation error");
-        return (-1);
-    }
+	if (num_chars == -1)
+	{
+		printf("Exit\n");
+		return (-1);
+	}
+	else
+	{
+		token = strtok(lineptr, delim);
+		while (token != NULL)
+		{
+			num_tokens++;
+			token = strtok(NULL, delim);
+		}
 
-    strcpy(lineptr_copy, lineptr);
+		num_tokens++;
 
-    if (num_chars == -1){
-        printf("Exiting shell...\n");
-        return (-1);
-    } else {
+		strings = malloc(sizeof(char *) * num_tokens);
+		token = strtok(lineptr_copy, delim);
 
-        token = strtok(lineptr, delim);
+		for (i = 0; token != NULL; i++)
+		{
+			strings[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(strings[i], token);
 
-        while (token != NULL){
-            n_tokens++;
-            token = strtok(NULL, delim);
-        }
-        n_tokens++;
+			token = strtok(NULL, delim);
+		}
 
-        argv = malloc(sizeof(char *) * n_tokens);
+		strings[i] = NULL;
 
-        token = strtok(lineptr_copy, delim);
+		free(strings);
+		free(lineptr);
+		free(lineptr_copy);
+	}
 
-        for (i = 0; token != NULL; i++){
-            argv[i] = malloc(sizeof(char) * strlen(token));
-            strcpy(argv[i], token);
-
-            token = strtok(NULL, delim);
-        }
-        argv[i] = NULL;
-
-
-
-        free(argv);
-        free(lineptr);
-        free(lineptr_copy);
-    }
-
-    return (0);
+	return (0);
 }
